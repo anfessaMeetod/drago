a5bf26f9efc92786d050b2f290582ba984e5a0e9
anfessa <anton@anfessa.com>
2017-05-16 20:08:14 +0200

    Added structure

--- index.php
+++ index.php
@@ -1,58 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Drago - Almost like Dragon, the seed</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Drago - Almost like Dragon, the seed</title>
 
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+  <link href="css/app.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <h1>Drago</h1>
+      <![endif]-->
+    </head>
+    <body>
+    <div class="container">
+        <section class="hero">
+          <div class="hero-inner">
+            <div id="backplate" class=" bg-black">
+              <div>
+                <img src="images/logo.png" alt="logo">
+              </div>
+            </div>
+            <div class="col-md-9 col-md-offset-3 block bg-green">
+             <img src="images/phone.png" class="floating-phone">
+             <h1>ACCESS TO GYMS
+              WITH THE TAP 
+              OF A FINGER</h1>
+              <hr>
+              <p>No memberships. No reservations. </p>
+              <p>No commitments. Workout on your terms.</p>
+              <img src="images/app_store_badge.png" class="badges">
+              <img src="images/google-play-badge.png" class="badges">
 
-    <section class="hero">
-      <h1>Hero</h1>
+            </div>
+          </div>
 
-    </section>
+        </section>
 
-    <section class="about">
-      <h2>About</h2>
+        <section class="about">
 
-    </section>
+          <div class="block bg-green">
 
-    <section class="how">
-      <h2>How it works</h2>
+            <div class="half">
+              <h1>LOCATE.
+                CHECK IN.
+                EXERCISE.
+                CHECK OUT.
+              </h1>
+              <hr>
+              <p>A membership with Bruce gets you access to a world of boutique fitness. Every studio in our network is handpicked based on their passion, knowledge and on being a leader.
+              </p>
+            </div>
+            <div class="half">
+              <img src="images/screens.png">
+            </div>
+          </div>
 
-    </section>
+        </section>
 
-    <section class="faq">
-      <h2>FAQ.</h2>
+        <section class="how">
 
-    </section>
+          <div class="flow">
+            <div class="half">
+            <div class="textarea">
+              <h1>How to drago</h1>
+              <p>DRAGO gives you access to local gyms, allowing you to workout on your own terms
+                and simply pay for the time spent. No memberships. No reservations.</p>
+                </div>
 
-    <section class="lets-talk">
-      <h2>Lets talk</h2>
+                <div class="screenholder bg-grey">
+                  <img src="images/app-2.png">
 
-    </section>
+                </div>
+                 <div class="textarea">
+                <h2>02 FIND YOUR WAY</h2>
+                <p>Find a gym that fits you. Whether it’s price, amenities or convenient location, it doesn’t matter, you decide.</p>
+                </div>
 
-    <footer>
-      <h2>Footer</h2>
-    </footer>
+                <div class="screenholder bg-grey">
+                  <img src="images/app-2.png">
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  </body>
-</html>
+                </div>
+                 <div class="textarea">
+                <h2>02 FIND YOUR WAY</h2>
+                <p>Find a gym that fits you. Whether it’s price, amenities or convenient location, it doesn’t matter, you decide.</p>
+                </div>
+              </div>
+
+              <div class="half">
+                <div class="screenholder bg-green">
+                  <img src="images/app-2.png">
+                </div>
+                <div class="textarea">
+                <h2>02 FIND YOUR WAY</h2>
+                <p>Find a gym that fits you. Whether it’s price, amenities or convenient location, it doesn’t matter, you decide.</p>
+                </div>
+                  <div class="screenholder bg-green">
+                    <img src="images/app-2.png">
+                  </div>
+                  <div class="textarea">
+                <h2>02 FIND YOUR WAY</h2>
+                <p>Find a gym that fits you. Whether it’s price, amenities or convenient location, it doesn’t matter, you decide.</p>
+                </div>
+                  </div>
+                </div>
+
+              </section>
+
+              <section class="faq">
+                <div class="block">
+              
+                  <div class="half">
+                    <h1>FAQ.</h1>
+                    <p>Can I use the classes offered at the gym?
+                  At this time you don’t have access to classes</p>
+                  <p>What if I lose phone service?
+For best service always ask for the gym WiFi, but in caseyou loose service don’t worry the system will still run inthe background.</p>
+                  </div>
+                  <div class="half">
+                    <p>What happens if I forget to check out?
+Make sure you always checkout, in case you forget or having issues the system is programmed to check you out manually. If
+you’re having issues please contact us</p>
+<p>How didn’t anyone think of this already? Super weird - we know, but enjoy!</p>
+                  </div>
+                </div>
+
+              </section>
+
+              <section class="lets-talk">
+               <div class="block">
+                 
+                 <div class="half">
+                   <h1>Let's talk</h1>
+                   <hr>
+                   <p>We want you to be a part of Dragos evolution. Our most valuable assets are our users. So if you have questions or feedback we will be happy to talk to you. So please, dont hesitate to write to us.  </p>
+                 </div>
+                  <div class="half">
+                   <form>
+                     <input type="text" name="">
+                   </form>
+                 </div>
+               </div>
+
+              </section>
+
+            </div>
+
+            <footer>
+              <h2>Footer</h2>
+            </footer>
+
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+          </body>
+          </html>
