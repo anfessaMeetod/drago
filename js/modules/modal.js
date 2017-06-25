53f57413a2a2e377f745f98dd55b7b744239ed74
anfessa <anton@anfessa.com>
2017-06-25 19:41:39 +0200

    Addad a comment to modal.js

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -24,8 +24,8 @@ closePrivacy.onclick = function() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        privacyModal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         privacyModal.style.display = "none";
+//     }
+// }
